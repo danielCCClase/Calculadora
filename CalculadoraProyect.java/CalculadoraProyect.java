6838f4a777a64eb09e27c6073624e446f7559547
danielCCClase <2904734@alu.murciaeduca.es>
2025-11-06 10:52:13 +0100

    Cambio para provocar conflicto

--- CalculadoraProyect.java/CalculadoraProyect.java
+++ CalculadoraProyect.java/CalculadoraProyect.java
@@ -1,2 +1,3 @@
 Esto es una practica de Entorno de Desarrollos
 Esto es una modificacion realizada directamente desde GITHUB
+Esto es una modificacion realizada directamente desde local para crear un conclicto
